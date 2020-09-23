202e694dcd17d862c83d64ce1aa7711133ce3241
Sam-PC\Sam <samueljireh.baquir@benilde.edu.ph>
2020-09-23 18:18:29 +0800

    Updated Quiz 1 contents. All sorting methods are programmed to display elements in descending order as per instruction of the quiz.

--- GDSTRUC Quiz 1/src/com/company/Main.java
+++ GDSTRUC Quiz 1/src/com/company/Main.java
@@ -4,23 +4,74 @@ public class Main {
 
     public static void main(String[] args) {
 
-        float[] floatNumbers = new float [20];
+        //The array to be sorted
+        int[] intNumbers = new int [10];
+        intNumbers[0] = 26;
+        intNumbers[1] = 126;
+        intNumbers[2] = 6;
+        intNumbers[3] = 2;
+        intNumbers[4] = -6;
+        intNumbers[5] = -2;
+        intNumbers[6] = -126;
+        intNumbers[7] = -26;
+        intNumbers[8] = 62;
+        intNumbers[9] = 16;
 
-        // Variable increment holds the value of each element
-        int increment = 1;
+        //Please uncomment only one of them :)
 
-        // Assigning values to each element
-        for (int i = 0; i < floatNumbers.length; i++)
+        //For using the BubbleSort in descending order (Largest to Smallest)
+        System.out.println("Before bubble sort:");
+        printArrayElement(intNumbers);
+        bubbleSort(intNumbers);
+        System.out.println("\n\nAfter bubble sort:");
+        printArrayElement(intNumbers);
+
+//        //For using the SelectionSort in descending order (Largest to Smallest)
+//        System.out.println("Before selection sort:");
+//        printArrayElement(intNumbers);
+//        selectionSort(intNumbers);
+//        System.out.println("\n\nAfter selection sort:");
+//        printArrayElement(intNumbers);
+    }
+
+    private static void bubbleSort(int[] arr)
+    {
+        for (int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
         {
-            floatNumbers[i] = increment;
-            increment++;
+            for (int i = 0; i < lastSortedIndex; i++)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
         }
+    }
 
-        // Print all contents of the array
-
-        for (int i = 0; i < floatNumbers.length; i++) {
-            System.out.println(floatNumbers[i]);
+    private static void selectionSort(int[] arr)
+    {
+        for (int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
+        {
+            int smallestIndex = 0;
+            for (int i = 1; i <= lastSortedIndex; i++)
+            {
+                if (arr[i] < arr[smallestIndex])
+                {
+                    smallestIndex = i;
+                }
+            }
+            int temp = arr[lastSortedIndex];
+            arr[lastSortedIndex] = arr[smallestIndex];
+            arr[smallestIndex] = temp;
         }
+    }
 
+    private static void printArrayElement(int[] arr)
+    {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
     }
 }
